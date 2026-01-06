1818abdfea7ba92db895f84c986320ee3f9ee743
shubh1855 <paulshubhranil1855@gmail.com>
2026-01-06 21:16:47 +0530

    Did some refactoring

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from stats import get_book_text
+
+book_path = "books/frankenstein.txt"
+
+get_book_text(book_path)
--- stats.py
+++ stats.py
@@ -1,11 +1,6 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as book:
+def get_book_text(book_path):
+    with open(book_path) as book:
         book_content = book.read()
         words = book_content.split()
-        count = 0
-        for word in words:
-            count += 1
+        count = len(words)
         print(f"Found {count} total words")
-
-
-get_book_text()
