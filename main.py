024cdafea7ca59055e73b7788ac79474193a34bd
shubh1855 <paulshubhranil1855@gmail.com>
2026-01-06 20:48:25 +0530

    Added count of words

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def get_book_text():
     with open("books/frankenstein.txt") as book:
         book_content = book.read()
-        print(book_content)
+        words = book_content.split()
+        count = 0
+        for word in words:
+            count += 1
+        print(f"Found {count} total words")
 
 
 get_book_text()
