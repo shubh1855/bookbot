7f63d7b814c907ee831065b123916544394b1c8d
shubh1855 <paulshubhranil1855@gmail.com>
2026-01-06 22:09:38 +0530

    Added proper sorting and format for the report

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ book_path = "books/frankenstein.txt"
 count, book_content = get_book_text(book_path)
 characters = get_char_dict(book_content)
 char_list = sorted_char_list(characters)
-print(char_list)
+
+print(f"---- Begin report of {book_path}")
+print(f"Found {count} total words in the book")
+for item in char_list:
+    if not item["char"].isalpha():
+        continue
+    print(f"{item['char']}: {item['num']}")
+print("----END of Report----")
