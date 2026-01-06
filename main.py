6b8b0afda31cb506f1640e90c9dde452be65a451
shubh1855 <paulshubhranil1855@gmail.com>
2026-01-06 21:38:56 +0530

    Added the list function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from stats import get_book_text
+from stats import get_book_text, get_char_dict, sorted_char_list
 
 book_path = "books/frankenstein.txt"
-
-get_book_text(book_path)
+count, book_content = get_book_text(book_path)
+characters = get_char_dict(book_content)
+char_list = sorted_char_list(characters)
+print(char_list)
--- stats.py
+++ stats.py
@@ -4,3 +4,27 @@ def get_book_text(book_path):
         words = book_content.split()
         count = len(words)
         print(f"Found {count} total words")
+        return count, book_content
+
+
+def get_char_dict(text):
+    chars = {}
+    for char in text:
+        lower_char = char.lower()
+        if lower_char in chars:
+            chars[lower_char] += 1
+        else:
+            chars[lower_char] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def sorted_char_list(chars):
+    sorted_list = []
+    for char in chars:
+        sorted_list.append({"char": char, "num": chars[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
